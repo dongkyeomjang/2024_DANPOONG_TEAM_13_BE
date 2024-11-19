26944f4a7077a6ac6eb8ca0380207c8a9915930d
장동겸 <jangelliot0404@dgu.ac.kr>
2024-11-19 17:56:59 +0900

    🐞 BugFix - 로그인 KakaoAccountDto 맵핑 오류 해결 (#40)

--- src/main/java/com/daon/onjung/security/info/KakaoOauth2UserInfo.java
+++ src/main/java/com/daon/onjung/security/info/KakaoOauth2UserInfo.java
@@ -10,49 +10,49 @@ public record KakaoOauth2UserInfo(
         @JsonProperty("connected_at")
         String connectedAt,
 
+        @JsonProperty("properties")
+        KakaoPropertiesDto properties,
+
         @JsonProperty("kakao_account")
         KakaoAccountDto kakaoAccount
 ) {
+    public record KakaoPropertiesDto(
+            @JsonProperty("nickname")
+            String nickname,
+
+            @JsonProperty("profile_image")
+            String profileImage,
+
+            @JsonProperty("thumbnail_image")
+            String thumbnailImage
+    ) {
+    }
+
     public record KakaoAccountDto(
+            @JsonProperty("profile_nickname_needs_agreement")
+            boolean profileNicknameNeedsAgreement,
+
+            @JsonProperty("profile_image_needs_agreement")
+            boolean profileImageNeedsAgreement,
+
             @JsonProperty("profile")
-            KakaoProfileDto profile,
-
-            @JsonProperty("name_needs_agreement")
-            boolean nameNeedsAgreement,
-
-            @JsonProperty("name")
-            String name,
-
-            @JsonProperty("has_email")
-            boolean hasEmail,
-
-            @JsonProperty("email_needs_agreement")
-            boolean emailNeedsAgreement,
-
-            @JsonProperty("is_email_valid")
-            boolean isEmailValid,
-
-            @JsonProperty("is_email_verified")
-            boolean isEmailVerified,
-
-            @JsonProperty("email")
-            String email,
-
-            @JsonProperty("has_phone_number")
-            boolean hasPhoneNumber,
-
-            @JsonProperty("phone_number_needs_agreement")
-            boolean phoneNumberNeedsAgreement,
-
-            @JsonProperty("phone_number")
-            String phoneNumber
+            KakaoProfileDto profile
     ) {
         public record KakaoProfileDto(
                 @JsonProperty("nickname")
                 String nickname,
 
+                @JsonProperty("thumbnail_image_url")
+                String thumbnailImageUrl,
+
                 @JsonProperty("profile_image_url")
-                String profileImageUrl
+                String profileImageUrl,
+
+                @JsonProperty("is_default_image")
+                boolean isDefaultImage,
+
+                @JsonProperty("is_default_nickname")
+                boolean isDefaultNickname
         ) {
         }
     }
