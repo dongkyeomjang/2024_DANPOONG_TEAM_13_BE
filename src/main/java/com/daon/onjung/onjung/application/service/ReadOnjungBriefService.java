9df6c95c181536cd83f0d136c60381638e5ef70d
장동겸 <jangelliot0404@dgu.ac.kr>
2024-11-24 05:30:45 +0900

    🐞 BugFix/#147 - 4.3 온기 간단정보 조회 온기 전달회수 버그 (#148)

--- src/main/java/com/daon/onjung/onjung/application/service/ReadOnjungBriefService.java
+++ src/main/java/com/daon/onjung/onjung/application/service/ReadOnjungBriefService.java
@@ -52,8 +52,8 @@ public class ReadOnjungBriefService implements ReadOnjungBriefUseCase {
         // 온정 생성
         Onjung onjung = onjungService.createOnjung(donations, receipts, shares);
 
-        // 온정 총 개수 계산 (가게별 중복 제외)
-        Integer totalOnjungCount = onjungService.calculateTotalUniqueOnjungStoreCount(onjung);
+        // 온정 총 개수 계산
+        Integer totalOnjungCount = donations.size() + receipts.size() + shares.stream().mapToInt(Share::getCount).sum();
 
         // 온정 총 금액 계산
         Integer totalOnjungAmount = onjungService.calculateTotalOnjungAmount(onjung);
