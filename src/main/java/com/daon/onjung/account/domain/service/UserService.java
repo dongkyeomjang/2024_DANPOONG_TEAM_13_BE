458139c939a96fdfbc01969a7ad376928684149b
장동겸 <jangelliot0404@dgu.ac.kr>
2024-11-27 22:50:50 +0900

    🔨 Refactor/#183 - 카카오 프로필 이미지 url 가공 (#184)

--- src/main/java/com/daon/onjung/account/domain/service/UserService.java
+++ src/main/java/com/daon/onjung/account/domain/service/UserService.java
@@ -13,6 +13,7 @@ public class UserService {
     public User createUser(KakaoOauth2UserInfo requestDto, String encodedPassword) {
         String profileImageUrl = Optional.ofNullable(requestDto.kakaoAccount().profile())
                 .map(KakaoOauth2UserInfo.KakaoAccountDto.KakaoProfileDto::profileImageUrl)
+                .map(url -> url.startsWith("http://") ? url.replace("http://", "https://") : url)
                 .orElse("NONE");
 
         String nickname = Optional.ofNullable(requestDto.kakaoAccount().profile())
